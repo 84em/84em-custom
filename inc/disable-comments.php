45fb1b64df015c1d0b831fa8f08a8bfa7f617455
Andrew Miller <andrew@84em.com>
2025-07-15 14:02:34 -0500

    Standardizes indentation from tabs to spaces
    Converts all tab characters to 4-space indentation across WordPress hook functions and callbacks for consistent code formatting
    
    Improves code readability and maintains uniform style throughout the codebase

--- inc/disable-comments.php
+++ inc/disable-comments.php
@@ -21,41 +21,41 @@ defined( 'ABSPATH' ) || exit;
 
 // Disable support for comments
 \add_action(
-	hook_name: 'init',
-	callback: function () {
-		$post_types = get_post_types();
-		foreach ( $post_types as $post_type ) {
-			if ( post_type_supports( $post_type, 'comments' ) ) {
-				remove_post_type_support( $post_type, 'comments' );
-				remove_post_type_support( $post_type, 'trackbacks' );
-			}
-		}
-	} );
+    hook_name: 'init',
+    callback: function () {
+        $post_types = get_post_types();
+        foreach ( $post_types as $post_type ) {
+            if ( post_type_supports( $post_type, 'comments' ) ) {
+                remove_post_type_support( $post_type, 'comments' );
+                remove_post_type_support( $post_type, 'trackbacks' );
+            }
+        }
+    } );
 
 // Close comments on the front-end
 \add_filter(
-	hook_name: 'comments_open',
-	callback: '__return_false',
-	priority: 20,
-	accepted_args: 2 );
+    hook_name: 'comments_open',
+    callback: '__return_false',
+    priority: 20,
+    accepted_args: 2 );
 
 // close Pings on the front-end
 \add_filter(
-	hook_name: 'pings_open',
-	callback: '__return_false',
-	priority: 20,
-	accepted_args: 2 );
+    hook_name: 'pings_open',
+    callback: '__return_false',
+    priority: 20,
+    accepted_args: 2 );
 
 // Hide existing comments
 \add_filter(
-	hook_name: 'comments_array',
-	callback: '__return_empty_array',
-	priority: 10,
-	accepted_args: 2 );
+    hook_name: 'comments_array',
+    callback: '__return_empty_array',
+    priority: 10,
+    accepted_args: 2 );
 
 // removes comments menu from the admin
 \add_action(
-	hook_name: 'admin_menu',
-	callback: function () {
-		remove_menu_page( 'edit-comments.php' );
-	} );
+    hook_name: 'admin_menu',
+    callback: function () {
+        remove_menu_page( 'edit-comments.php' );
+    } );
--- inc/document-title.php
+++ inc/document-title.php
@@ -19,13 +19,13 @@ namespace EightyFourEM;
 defined( 'ABSPATH' ) || exit;
 
 \add_filter(
-	hook_name: 'document_title',
-	callback: function ( $title ) {
-		$_genesis_title = \get_post_meta( \get_the_ID(), '_genesis_title', true );
-		if ( ! empty( $_genesis_title ) ) {
-			$title = \wp_strip_all_tags( $_genesis_title );
-		}
+    hook_name: 'document_title',
+    callback: function ( $title ) {
+        $_genesis_title = \get_post_meta( \get_the_ID(), '_genesis_title', true );
+        if ( ! empty( $_genesis_title ) ) {
+            $title = \wp_strip_all_tags( $_genesis_title );
+        }
 
-		return $title;
-	},
-	priority: 100 );
+        return $title;
+    },
+    priority: 100 );
